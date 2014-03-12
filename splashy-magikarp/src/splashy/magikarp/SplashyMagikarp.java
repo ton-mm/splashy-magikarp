8e82a92c6cbeaf84e6d8205c99da694c8747a537
Tony Stark <tonmm92@gmail.com>
2014-03-11 18:45:24 -0600

    splashy

---
+++ splashy-magikarp/src/splashy/magikarp/SplashyMagikarp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package splashy.magikarp;
+
+/**
+ *
+ * @author tonystark
+ */
+public class SplashyMagikarp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
